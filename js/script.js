c08509daf8c3907a5a018169f304152f2db105be
Alexandre Saxemard <alexandre.saxemard@crous-nice.fr>
2023-10-30 14:54:29 +0100

    Ajout de commentaires

--- js/script.js
+++ js/script.js
@@ -1,27 +1,40 @@
 $(document).ready(function () {
+  // Créer un ensemble pour stocker les utilisateurs sélectionnés
   const selectedUsers = new Set();
+
+  // Sélectionner le bouton de soumission
   const $submitButton = $("#submitButton");
 
-  // Initialisez la liste déroulante comme une liste Select2
+  // Initialiser la liste déroulante comme une liste Select2
   $("#userSelect").select2();
 
-  // Gérez les événements de changement
+  // Gérer les événements de changement dans la liste déroulante
   $("#userSelect").on("change", function () {
+    // Récupérer les options sélectionnées ou un tableau vide
     const selectedOptions = $(this).val() || [];
+
+    // Vider l'ensemble des utilisateurs sélectionnés
     selectedUsers.clear();
 
+    // Ajouter les ID des utilisateurs sélectionnés à l'ensemble
     selectedOptions.forEach(function (userId) {
       selectedUsers.add(userId);
     });
 
+    // Mettre à jour la valeur d'un champ d'entrée avec les utilisateurs sélectionnés
     $("#selectedUsersInput").val(Array.from(selectedUsers).join(","));
+
+    // Vérifier l'état du bouton de téléchargement
     checkDownloadButtonState();
   });
 
+  // Fonction pour vérifier l'état du bouton de téléchargement
   function checkDownloadButtonState() {
+    // Si aucun utilisateur n'est sélectionné, désactiver le bouton
     if (selectedUsers.size === 0) {
       $submitButton.prop("disabled", true);
     } else {
+      // Sinon, activer le bouton
       $submitButton.prop("disabled", false);
     }
   }
--- process.php
+++ process.php
@@ -1,55 +1,76 @@
 <?php
+// Inclure le fichier de configuration de la base de données
 include('db.php');
 
+// Récupérer les utilisateurs sélectionnés depuis le formulaire et les convertir en un tableau
 $selected_users = isset($_POST['users']) ? explode(',', $_POST['users']) : [];
 
-// Ajoutez cette vérification au début de votre fichier process.php
+// Vérifier si des utilisateurs ont été sélectionnés
 if (empty($selected_users)) {
     echo "Aucun utilisateur n'a été sélectionné. Veuillez sélectionner au moins un utilisateur.";
     exit;
 }
 
 try {
+    // Initialisation du contenu du fichier texte
     $file_content = "";
 
+    // Parcourir la liste des utilisateurs sélectionnés
     foreach ($selected_users as $selected_user) {
+        // Vérifier si l'utilisateur est un contact en vérifiant s'il commence par 'contact_'
         if (strpos($selected_user, 'contact_') === 0) {
+            // Si c'est un contact, récupérer le nom du contact (en supprimant 'contact_')
             $contact = str_replace('contact_', '', $selected_user);
+
+            // Construire la requête SQL pour récupérer les ordinateurs liés à ce contact
             $sql = "SELECT c.id, c.name AS computer_name
                     FROM glpi_computers c
                     WHERE c.contact = ?";
+
+            // Préparation de la requête SQL avec un paramètre pour le nom du contact
             $stmt = $conn->prepare($sql);
             $stmt->bind_param("s", $contact);
-            $user_name = $contact; // Utilisez la valeur du contact ici
+            $user_name = $contact; // Utilisez la valeur du contact comme nom d'utilisateur
 
         } else {
+            // Si c'est un utilisateur, récupérer les ordinateurs liés à cet utilisateur
             $sql = "SELECT c.id, c.name AS computer_name
                     FROM glpi_computers c
                     WHERE c.users_id = ?";
+
+            // Préparation de la requête SQL avec un paramètre pour l'ID de l'utilisateur
             $stmt = $conn->prepare($sql);
             $stmt->bind_param("i", $selected_user);
-            $user_name = fetch_user_name_by_id($selected_user); // Utilisez la fonction pour les utilisateurs ici
+            $user_name = fetch_user_name_by_id($selected_user); // Utilisez la fonction pour récupérer le nom de l'utilisateur
         }
 
+        // Exécution de la requête SQL
         $stmt->execute();
+
+        // Récupération des résultats de la requête sous forme de tableau associatif
         $computers = $stmt->get_result()->fetch_all(MYSQLI_ASSOC);
 
+        // Vérifier si des ordinateurs ont été trouvés pour cet utilisateur/contact
         if (!empty($computers)) {
+            // Ajouter le nom de l'utilisateur/contact et la liste des ordinateurs au contenu du fichier
             $file_content .= "Liste des ordinateurs de l'utilisateur '$user_name':\n";
             foreach ($computers as $computer) {
                 $line = $computer['computer_name'] . "\n";
                 $file_content .= $line;
             }
-            $file_content .= "\n";
+            $file_content .= "\n"; // Ajouter une ligne vide pour séparer les utilisateurs/contacts
         }
     }
 
+    // Vérifier si des données ont été trouvées
     if (empty($file_content)) {
-        echo "Aucun ordinateur n'a été trouvé pour les utilisateurs sélectionnés.";
+        echo "Aucun ordinateur n'a été trouvé pour les utilisateurs/contacts sélectionnés.";
     } else {
+        // Génération du fichier texte
         $file_path = "computers.txt";
         file_put_contents($file_path, $file_content);
 
+        // Configurer les en-têtes pour forcer le téléchargement du fichier
         header('Content-Description: File Transfer');
         header('Content-Type: text/plain');
         header('Content-Disposition: attachment; filename="' . basename($file_path) . '"');
@@ -57,6 +78,8 @@ try {
         header('Cache-Control: must-revalidate');
         header('Pragma: public');
         header('Content-Length: ' . filesize($file_path));
+
+        // Envoyer le contenu du fichier au navigateur et terminer le script
         flush();
         readfile($file_path);
         exit;
@@ -75,7 +98,7 @@ function fetch_user_name_by_id($user_id)
         // Construire la requête SQL pour récupérer le nom de l'utilisateur par son ID
         $sql = "SELECT name FROM glpi_users WHERE id = ?";
 
-        // Exécution de la requête en utilisant un paramètre pour l'ID de l'utilisateur
+        // Préparation de la requête SQL avec un paramètre pour l'ID de l'utilisateur
         $stmt = $conn->prepare($sql);
         $stmt->bind_param("i", $user_id); // "i" indique que c'est un entier (ID d'utilisateur)
         $stmt->execute();
@@ -96,3 +119,4 @@ function fetch_user_name_by_id($user_id)
         $conn->close();
     }
 }
+?>
