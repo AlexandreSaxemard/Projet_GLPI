ecee1bc7cd78390817d2dd7f3b313d9e7941396e
Alexandre Saxemard <alexandre.saxemard@crous-nice.fr>
2023-10-31 08:36:21 +0100

    Rajout de la fonctionnalité qui récupère les données des contacts (users_id = 0) + remise en forme

--- process.php
+++ process.php
@@ -1,79 +1,86 @@
 <?php
-// Inclure le fichier de configuration de la base de données
+// Inclut le fichier de configuration de la base de données
 include('db.php');
 
-// Récupérer les utilisateurs sélectionnés depuis le formulaire et les convertir en un tableau
+// Récupère les utilisateurs sélectionnés depuis le formulaire et les convertit en un tableau
 $selected_users = isset($_POST['users']) ? explode(',', $_POST['users']) : [];
 
-// Vérifier si des utilisateurs ont été sélectionnés
+// Vérifie si des utilisateurs ont été sélectionnés
 if (empty($selected_users)) {
     echo "Aucun utilisateur n'a été sélectionné. Veuillez sélectionner au moins un utilisateur.";
     exit;
 }
 
 try {
-    // Initialisation du contenu du fichier texte
+    // Initialise le contenu du fichier texte
     $file_content = "";
 
-    // Parcourir la liste des utilisateurs sélectionnés
+    // Parcourt la liste des utilisateurs sélectionnés
     foreach ($selected_users as $selected_user) {
-        // Vérifier si l'utilisateur est un contact en vérifiant s'il commence par 'contact_'
+        // Vérifie si l'élément est un contact en cherchant la chaîne 'contact_'
         if (strpos($selected_user, 'contact_') === 0) {
-            // Si c'est un contact, récupérer le nom du contact (en supprimant 'contact_')
+            // Supprime 'contact_' pour obtenir seulement le nom du contact
             $contact = str_replace('contact_', '', $selected_user);
 
-            // Construire la requête SQL pour récupérer les ordinateurs liés à ce contact
-            $sql = "SELECT c.id, c.name AS computer_name
+            // Modifie la requête SQL pour inclure serial, date_mod et date_creation
+            // Ajoute également une condition pour que users_id soit égal à 0
+            $sql = "SELECT c.id, c.name AS computer_name, c.serial, c.date_mod, c.date_creation 
                     FROM glpi_computers c
-                    WHERE c.contact = ?";
+                    WHERE c.contact = ? AND c.users_id = 0";
 
-            // Préparation de la requête SQL avec un paramètre pour le nom du contact
+            // Prépare la requête SQL
             $stmt = $conn->prepare($sql);
             $stmt->bind_param("s", $contact);
-            $user_name = $contact; // Utilisez la valeur du contact comme nom d'utilisateur
-
+            $user_name = $contact; // Utilise le nom du contact comme nom d'utilisateur
         } else {
-            // Si c'est un utilisateur, récupérer les ordinateurs liés à cet utilisateur
-            $sql = "SELECT c.id, c.name AS computer_name, c.serial, c.date_mod, c.date_creation
+            // Si ce n'est pas un contact, alors c'est un utilisateur. 
+            // Récupère les informations correspondantes
+            $sql = "SELECT c.id, c.name AS computer_name, c.serial, c.date_mod, c.date_creation 
                     FROM glpi_computers c
                     WHERE c.users_id = ?";
 
-            // Préparation de la requête SQL avec un paramètre pour l'ID de l'utilisateur
+            // Prépare la requête SQL
             $stmt = $conn->prepare($sql);
             $stmt->bind_param("i", $selected_user);
-            $user_name = fetch_user_name_by_id($selected_user); // Utilisez la fonction pour récupérer le nom de l'utilisateur
+            $user_name = fetch_user_name_by_id($selected_user); // Utilise une fonction pour récupérer le nom d'utilisateur
         }
 
-        // Exécution de la requête SQL
+        // Exécute la requête SQL
         $stmt->execute();
-
-        // Récupération des résultats de la requête sous forme de tableau associatif
+        // Récupère les résultats dans un tableau associatif
         $computers = $stmt->get_result()->fetch_all(MYSQLI_ASSOC);
 
-        // Vérifier si des ordinateurs ont été trouvés pour cet utilisateur/contact
+        // Si des ordinateurs ont été trouvés pour cet utilisateur/contact
         if (!empty($computers)) {
-            // Ajouter le nom de l'utilisateur/contact et la liste des ordinateurs au contenu du fichier
             $file_content .= "Liste des ordinateurs de l'utilisateur '$user_name':\n\n";
             foreach ($computers as $computer) {
-                $line = "Nom de l'ordinateur : " . $computer['computer_name'] . "\n";
-                $line .= "Numéro de série : " . $computer['serial'] . "\n";
-                $line .= "Date de modification : " . $computer['date_mod'] . "\n";
-                $line .= "Date de création : " . $computer['date_creation'] . "\n";
+                // Construit une ligne de texte pour chaque ordinateur
+                $line = "Nom de l'ordinateur : " . $computer['computer_name'] . ", ";
+                if (isset($computer['serial'])) {
+                    $line .= "Numéro de série : " . $computer['serial'] . ", ";
+                }
+                if (isset($computer['date_mod'])) {
+                    $line .= "Date de modification : " . $computer['date_mod'] . ", ";
+                }
+                if (isset($computer['date_creation'])) {
+                    $line .= "Date de création : " . $computer['date_creation'];
+                }
+                $line .= "\n";
                 $file_content .= $line;
             }
-            $file_content .= "\n"; // Ajouter une ligne vide pour séparer les utilisateurs/contacts
+            $file_content .= "\n"; // Ajoute une ligne vide pour séparer les utilisateurs/contacts
         }
     }
 
-    // Vérifier si des données ont été trouvées
+    // Vérifie si des données ont été trouvées
     if (empty($file_content)) {
         echo "Aucun ordinateur n'a été trouvé pour les utilisateurs/contacts sélectionnés.";
     } else {
-        // Génération du fichier texte
+        // Génère le fichier texte
         $file_path = "computers.txt";
         file_put_contents($file_path, $file_content);
 
-        // Configurer les en-têtes pour forcer le téléchargement du fichier
+        // Configure les en-têtes pour forcer le téléchargement du fichier
         header('Content-Description: File Transfer');
         header('Content-Type: text/plain');
         header('Content-Disposition: attachment; filename="' . basename($file_path) . '"');
@@ -82,43 +89,45 @@ try {
         header('Pragma: public');
         header('Content-Length: ' . filesize($file_path));
 
-        // Envoyer le contenu du fichier au navigateur et terminer le script
+        // Envoie le contenu du fichier au navigateur et termine le script
         flush();
         readfile($file_path);
         exit;
     }
 } catch (Exception $e) {
+    // Affiche un message d'erreur en cas d'exception
     echo "Une erreur s'est produite : " . $e->getMessage();
 }
 
-// Fonction pour récupérer le nom de l'utilisateur par son ID
+// Fonction pour récupérer le nom de l'utilisateur en fonction de son ID
 function fetch_user_name_by_id($user_id)
 {
-    // Inclure le fichier de configuration de la base de données
+    // Inclut à nouveau le fichier de configuration de la base de données
     include('db.php');
 
     try {
-        // Construire la requête SQL pour récupérer le nom de l'utilisateur par son ID
+        // Construit la requête SQL pour récupérer le nom de l'utilisateur par son ID
         $sql = "SELECT name FROM glpi_users WHERE id = ?";
 
-        // Préparation de la requête SQL avec un paramètre pour l'ID de l'utilisateur
+        // Prépare la requête SQL
         $stmt = $conn->prepare($sql);
         $stmt->bind_param("i", $user_id); // "i" indique que c'est un entier (ID d'utilisateur)
         $stmt->execute();
         $result = $stmt->get_result();
 
-        // Vérifier si un résultat a été trouvé
+        // Si un résultat est trouvé, renvoie le nom de l'utilisateur
         if ($row = $result->fetch_assoc()) {
-            // Récupérer le nom de l'utilisateur depuis le résultat de la requête
             $user_name = $row['name'];
             return $user_name;
         } else {
-            return "Utilisateur inconnu"; // Si l'ID d'utilisateur n'est pas trouvé dans la base de données
+            // Si aucun résultat n'est trouvé, renvoie "Utilisateur inconnu"
+            return "Utilisateur inconnu";
         }
     } catch (Exception $e) {
+        // En cas d'erreur, renvoie un message d'erreur
         return "Erreur lors de la récupération du nom d'utilisateur : " . $e->getMessage();
     } finally {
-        // Fermeture de la connexion à la base de données
+        // Ferme la connexion à la base de données
         $conn->close();
     }
 }
